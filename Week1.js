51362f880656a45287207aae80c7629e30e32a2b
Jaseela99 <jaseelachittur@gmail.com>
2022-08-16 23:15:49 +0530

    added plusminus

--- Week1.js
+++ Week1.js
@@ -182,3 +182,29 @@ let result = ''
     }
     console.log(result)
 }
+//////////////////////////////////////
+
+
+/* Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.
+ Print the decimal value of each fraction on a new line with 6 places after the decimal. */
+
+ function plusMinus(arr) {
+    // Write your code here
+    let posCount=0;
+    let negCount=0;
+    let zeroCount=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<0){
+            negCount++
+        }else if(arr[i]>0){
+            posCount++
+        }else if(arr[i]===0){
+            zeroCount++
+        }
+    }
+    console.log(posCount/arr.length)
+    console.log(negCount/arr.length)
+    console.log(zeroCount/arr.length)
+    
+
+}
