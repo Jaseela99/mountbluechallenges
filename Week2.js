08a7e4fb941bd76f2db2362520062d23e1a8f073
Jaseela99 <jaseelachittur@gmail.com>
2022-08-21 22:26:42 +0530

    added caeser cipher

--- Week2.js
+++ Week2.js
@@ -10,3 +10,27 @@ function miniMaxSum(arr) {
    let max = newArr.slice(1).reduce((a,b)=>a+b) //removes the first element => 3,5,7,9 
    console.log(min, max)
    }
+
+   //////////////////////////////////
+/*    Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc */
+
+function caesarCipher(s, k) {
+    // Write your code here
+  const lowerA = "abcdefghijklmnopqrstuvwxyz"
+  const upperA = lowerA.toUpperCase()
+
+  const res = s.split("").map(c => {
+    if (lowerA.includes(c)) {
+      return lowerA[(lowerA.indexOf(c) + k) % 26]
+    } else if (upperA.includes(c)) {
+      return upperA[(upperA.indexOf(c) + k) % 26]
+    } else {
+      return c
+    }
+  })
+
+  return res.join("")
+}
